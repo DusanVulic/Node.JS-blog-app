c089cd36d1d58155f81786ba670b49a67aaec04d
Dusan <dusanvulic11@gmail.com>
2022-07-15 12:01:36 +0200

    Express app created

--- app.js
+++ app.js
@@ -13,5 +13,25 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
     //res send salje ono sto zelim da vidim na /
     // res send automatski setuje status 200 i automatski kreira content-type
-    res.send("<p>Home page</p>");
+    //
+    //res.send("<p>Home page</p>");
+
+    // a mogu da saljem i fajlove
+    //ali moram da kazem odakle dolazi taj fajl
+    res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// redirektovanje
+app.get("/about-us", (req, res) => {
+    res.redirect("/about");
+});
+
+// 404
+//mora ici na dno fajla
+app.use((req, res) => {
+    res.status(404).sendFile("./views/error.html", { root: __dirname });
 });

bb41fa6235f0636fad6d561cff16cf0b2d8ef386
Dusan <dusanvulic11@gmail.com>
2022-07-16 12:51:25 +0200

    JS functionality removen for this time

--- public/functions.js
+++
@@ -1,7 +0,0 @@
-console.log("functions connection checkout");
-
-const testButton = document.querySelector(".test");
-
-testButton.addEventListener("click", () => {
-    console.log("pogodjen si ");
-});
